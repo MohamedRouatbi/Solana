3d2cf6c9f24c71dad87dca6801e118a3750b6dda
Mohamed <mohamed.rouatbi@eniso.u-sousse.tn>
2025-07-08 00:18:17 +0100

    first commit

---
+++ favorites/programs/favorites/src/lib.rs
@@ -0,0 +1,23 @@
+use anchor_lang::prelude::*;
+
+declare_id!("");
+
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize=8 ;
+#[program]
+
+pub mod favorites{
+    use super::*;
+
+    pub fn set_favorites() -> Return<()>{
+        //we will fill in later 
+    }
+}
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    pub number : u64 ,
+    #[max_len(50)]
+    pub color : String,
+    #[max_len(5,50)]
+    pub hobbies : Vec<String>,
+}
